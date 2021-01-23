9e20838868d540fcebdc337ec78cdf180f2004de
Chase Wilson <me@chasewilson.dev>
2021-01-23 11:04:26 -0600

    Broadcast Stop to all workers

--- rust/template/differential_datalog/src/program/mod.rs
+++ rust/template/differential_datalog/src/program/mod.rs
@@ -1604,7 +1604,7 @@ impl RunningProgram {
         }
 
         self.flush()
-            .and_then(|_| self.send(0, Msg::Stop))
+            .and_then(|_| self.broadcast(Msg::Stop))
             .and_then(|_| {
                 self.worker_guards.take().map_or(Ok(()), |worker_guards| {
                     worker_guards
@@ -1628,7 +1628,7 @@ impl RunningProgram {
         }
 
         self.transaction_in_progress = true;
-        Result::Ok(())
+        Ok(())
     }
 
     /// Commit a transaction.
